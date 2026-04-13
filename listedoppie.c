5b025423df1ffcdc676e4e73f97586fcb29f3fb1
lorena <you@example.com>
2026-04-13 12:27:19 +0200

    cambio

--- listedoppie.c
+++ listedoppie.c
@@ -4,16 +4,44 @@
 
 struct elem
 {
-    int info;
-    struct elem* perc;
-    struct elem* next;
+    char info;
+    struct elem* prev;
+    struct elem* pun;
 };
 typedef struct elem* lista; 
 
 int head (lista m){ return m->info;};
 
-lista tail (lista m){ return m->next; };
+lista tail (lista m){ return m->pun; };
+
+lista delete_elem(lista l, struct elem* e){
+    if (l==e)
+    {
+        l=tail(l);
+    }
+    else
+    {
+        (e->prev)->pun = e->pun;
+    }
+    if (e->pun != NULL)
+    {
+        (e->pun)->prev = e->prev;
+    }
+    free (e);
+    return l;  
+};
+
+lista insert_elem(lista l, struct elem* e){
+    e->pun = l;
+    if (l != NULL)
+    {
+        l->prev = e;
+    }
+    e->prev = NULL; //posso evitare di farla s eho già elento allocato ho già anche NULL???
+    return e;       
+};
 
 int main(){
+    //potrebbe essere utile fare una prova grafica per capire cosa fare  ei vari casi, basta lista di tre elementi
     return 1;
 }
