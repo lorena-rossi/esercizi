ff25b4ecc4293971ef45e6acb62e0d75dc3cbff7
lorena <lorenarossi.00@gmail.com>
2026-04-16 12:25:53 +0200

    esempio collegamento esterno

---
+++ moduli/esercizio_collegamenti/file.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std ;
+
+extern int a;
+
+void fun(){
+
+    cout<<"la variabile esterna vale : "<<a<<endl;
+};
---
+++ moduli/esercizio_collegamenti/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int a;
+void fun();
+
+int main(){
+
+    cout<<"Inserire valore variabile: ";
+    cin>>a;
+    fun();
+    return 0;
+}
