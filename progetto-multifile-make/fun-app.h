a5b7e39d2295f130098b3e6a45887426016fc7de
lorena <lorenarossi.00@gmail.com>
2026-04-30 11:35:47 +0200

    sistema

--- progetto-multifile-make/fun-app.h
+++
@@ -1,7 +0,0 @@
-/**********************************************/
-/* HEADER MODULO "funzioni dell'applicazione" */
-/**********************************************/
-void stampalista(lista);
-lista crealista(int, bool);
-lista cancella(lista, tipo_inf);
-void naviga(elem*);
