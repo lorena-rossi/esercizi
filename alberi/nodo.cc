1c7c1853530334f70a9338261985bf24d1b5789b
lorena <lorenarossi.00@gmail.com>
2026-05-07 12:23:29 +0200

    aggiornamenti

---
+++ alberi/nodo.cc
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+#include "nodo.h"
+
+node* new_node( tipo_inf i){
+    tree n = malloc( sizeof( tree ));
+}
+
+int main(){
+
+    return 0;
+}
---
+++ alberi/nodo.h
@@ -0,0 +1,9 @@
+
+typedef struct nodo{
+    tipo_inf inf;
+    node* parent;
+    node* firstchild;
+    node* nextsibling;
+} tree;
+ 
+//tree root;
