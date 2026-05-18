dfb386c82200e7e83544194d2b749bf1a4025a66
lorena <lorenarossi.00@gmail.com>
2026-05-18 12:03:27 +0200

    implemento grafi

---
+++ grafi/grafi.h
@@ -0,0 +1,28 @@
+struct adj_node
+{
+    int node;
+    float weight;
+    adj_node* next;
+};
+
+typedef adj_node* adj_list;
+
+
+typedef struct {
+    adj_list* nodes;
+    int dim;
+}graph;
+
+
+int get_dim(graph);
+adj_list get_adjlist(graph, int);
+
+int get_adjnode(adj_node*);
+adj_list get_nextadj( adj_list);
+float get_weight(adj_list);
+
+
+graph new_graph( int );
+void add_arc(graph& , int, int, float); //grafo orientato 
+void add_edge(graph& , int, int, float); // grafo non orientato 
+
---
+++ grafi/main.cc
@@ -0,0 +1,19 @@
+#include "grafi.h"
+
+#include <fstream> 
+#include <iostream>
+
+using namespace std;
+
+graph g_build( ifstream &g, bool d, bool w){
+    // legge da g nuemero di nodi(prima riga) archi da aggiungere sono righe successive 
+    // se w è true allora ha i pesi, se d serve per capire se è orientato
+    fopen;
+};
+
+
+
+int main(int argc, char* argv[]){
+
+    return 1;
+}
