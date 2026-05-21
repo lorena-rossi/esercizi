2ab70699d1941a48ca00d385ca7a29478f5551df
lorena <lorenarossi.00@gmail.com>
2026-05-21 10:49:52 +0200

    aggiornamento

--- grafi/main.cc
+++ grafi/main.cc
@@ -9,7 +9,7 @@ using namespace std;
 graph g_build( ifstream &g, bool d, bool w){
     // legge da g nuemero di nodi(prima riga) archi da aggiungere sono righe successive 
     // se w è true allora ha i pesi, se d serve per capire se è orientato
-    fopen;
+    
 };
 
 bool visitagraph(graph g, adj_list v){
@@ -17,7 +17,7 @@ bool visitagraph(graph g, adj_list v){
 };
 
 bool connected(graph g){
-    //connesso se reggiunto vero per ogni vertice 
+    //connesso se raggiunto vero per ogni vertice 
     
 };
 
